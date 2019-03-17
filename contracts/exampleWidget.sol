075772a855e52bb6ac99f617eb9fe7f8f1e17f7f
Rob Hitchens <26111999+rob-Hitchens@users.noreply.github.com>
2019-03-17 13:13:48 -0600

    Update exampleWidget.sol

--- contracts/exampleWidget.sol
+++ contracts/exampleWidget.sol
@@ -16,10 +16,11 @@ contract Widget {
     mapping(bytes32 => WidgetStruct) widgets;
     
     event LogNewWidget(address sender, bytes32 key, string name, bool delux, uint price);
+    event LogUpdateWidget(address sender, bytes32 key, string name, bool delux, uint price);    
     event LogRemWidget(address sender, bytes32 key);
     
     function newWidget(bytes32 key, string memory name, bool delux, uint price) public {
-        widgetSet.insert(key);
+        widgetSet.insert(key); // Note that this will fail automatically if the key already exists.
         WidgetStruct storage w = widgets[key];
         w.name = name;
         w.delux = delux;
@@ -27,14 +28,23 @@ contract Widget {
         emit LogNewWidget(msg.sender, key, name, delux, price);
     }
     
+    function updateWidget(bytes32 key, string memory name, bool delux, uint price) public {
+        require(widgetSet.exists(key), "Can't update a widget that doesn't exist.");
+        WidgetStruct storage w = widgets[key];
+        w.name = name;
+        w.delux = delux;
+        w.price = price;
+        emit LogUpdateWidget(msg.sender, key, name, delux, price);
+    }
+    
     function remWidget(bytes32 key) public {
-        widgetSet.remove(key);
+        widgetSet.remove(key); // Note that this will fail automatically if the key doesn't exist
         delete widgets[key];
         emit LogRemWidget(msg.sender, key);
     }
     
     function getWidget(bytes32 key) public view returns(string memory name, bool delux, uint price) {
-        require(widgetSet.exists(key));
+        require(widgetSet.exists(key), "Can't get a widget that doesn't exist.");
         WidgetStruct storage w = widgets[key];
         return(w.name, w.delux, w.price);
     }
