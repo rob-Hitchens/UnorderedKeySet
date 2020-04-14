a11998ad1c3b4941863ee723fa461d38c07c899f
Tjelvar Guo <=>
2020-04-14 12:04:12 +0200

    Update to solidity v.0.6.0

--- contracts/HitchensUnorderedAddressSet.sol
+++ contracts/HitchensUnorderedAddressSet.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.1; 
+pragma solidity ^0.6.0; 
 
 /* 
 Hitchens UnorderedAddressSet v0.93
@@ -31,18 +31,19 @@ THIS SOFTWARE IS NOT TESTED OR AUDITED. DO NOT USE FOR PRODUCTION.
 */
 
 library HitchensUnorderedAddressSetLib {
-    
+
     struct Set {
         mapping(address => uint) keyPointers;
         address[] keyList;
     }
-    
+
     function insert(Set storage self, address key) internal {
         require(key != address(0), "UnorderedKeySet(100) - Key cannot be 0x0");
         require(!exists(self, key), "UnorderedAddressSet(101) - Address (key) already exists in the set.");
-        self.keyPointers[key] = self.keyList.push(key)-1;
+        self.keyList.push(key);
+        self.keyPointers[key] = self.keyList.length-1;
     }
-    
+
     function remove(Set storage self, address key) internal {
         require(exists(self, key), "UnorderedKeySet(102) - Address (key) does not exist in the set.");
         address keyToMove = self.keyList[count(self)-1];
@@ -50,22 +51,22 @@ library HitchensUnorderedAddressSetLib {
         self.keyPointers[keyToMove] = rowToReplace;
         self.keyList[rowToReplace] = keyToMove;
         delete self.keyPointers[key];
-        self.keyList.length--;
+        self.keyList.pop();
     }
-    
+
     function count(Set storage self) internal view returns(uint) {
         return(self.keyList.length);
     }
-    
+
     function exists(Set storage self, address key) internal view returns(bool) {
         if(self.keyList.length == 0) return false;
         return self.keyList[self.keyPointers[key]] == key;
     }
-    
+
     function keyAtIndex(Set storage self, uint index) internal view returns(address) {
         return self.keyList[index];
     }
-    
+
     function nukeSet(Set storage self) public {
         delete self.keyList;
     }
