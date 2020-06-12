9e17a97efed8fbed9b6cebc389e67bac41e497f9
Paul Razvan Berg <hello@paulrberg.com>
2020-06-12 12:12:22 +0300

    feat: upgrade to solidity ^0.6.0

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,22 +1,23 @@
-pragma solidity 0.5.1;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
 
 /// @dev Stub for access control.
 
 contract Ownable {
-    
+
     address public owner;
-    
+
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
-    
+
     event LogNewOwner(address sender, address newOwner);
-    
+
     constructor() public {
         owner = msg.sender;
     }
-    
+
     function changeOwner(address newOwner) public onlyOwner {
         require(newOwner != address(0));
         emit LogNewOwner(msg.sender, newOwner);
