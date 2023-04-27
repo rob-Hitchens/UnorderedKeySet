009277802bbfae089364ca3ccd38963cbb0a619e
Brendon Rofe <b1@xgmi.com>
2023-04-27 16:10:29 +0200

    Update to 0.8.4; optimize error handling for gas

--- contracts/HitchensUnorderedKeySet.sol
+++ contracts/HitchensUnorderedKeySet.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.4;
 
 /*
 Hitchens UnorderedKeySet v0.93
@@ -33,20 +33,30 @@ THIS SOFTWARE IS NOT TESTED OR AUDITED. DO NOT USE FOR PRODUCTION.
 
 library HitchensUnorderedKeySetLib {
 
+    error UnorderedKeySet100KeyCannotBe0x0();
+    error UnorderedKeySet101KeyAlreadyExistsInSet();
+    error UnorderedKeySet102KeyDoesNotExistInSet();
+
     struct Set {
         mapping(bytes32 => uint) keyPointers;
         bytes32[] keyList;
     }
 
     function insert(Set storage self, bytes32 key) internal {
-        require(key != 0x0, "UnorderedKeySet(100) - Key cannot be 0x0");
-        require(!exists(self, key), "UnorderedKeySet(101) - Key already exists in the set.");
+        if(key == 0x0) {
+            revert UnorderedKeySet100KeyCannotBe0x0();
+        }
+        if(exists(self, key)) {
+            revert UnorderedKeySet101KeyAlreadyExistsInSet();
+        }
         self.keyList.push(key);
         self.keyPointers[key] = self.keyList.length - 1;
     }
 
     function remove(Set storage self, bytes32 key) internal {
-        require(exists(self, key), "UnorderedKeySet(102) - Key does not exist in the set.");
+        if(!exists(self, key)) {
+            revert UnorderedKeySet102KeyDoesNotExistInSet();
+        }
         bytes32 keyToMove = self.keyList[count(self)-1];
         uint rowToReplace = self.keyPointers[key];
         self.keyPointers[keyToMove] = rowToReplace;
@@ -100,5 +110,9 @@ contract HitchensUnorderedKeySet {
 
     function keyAtIndex(uint index) public view returns(bytes32) {
         return set.keyAtIndex(index);
+    }
+
+    function stringToBytes(string memory _string) external pure returns (bytes32) {
+        return keccak256(abi.encode(_string));
     }
 }
